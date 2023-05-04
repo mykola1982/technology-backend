9c94aeb253cc0b10696a600112064ea33ec8968b
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2023-05-04 22:14:33 +0300

    add looger, cors, parser

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const logger = require("morgan");
+require("dotenv").config();
 
 const app = express();
+const cors = require("cors");
 
+const formatLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatLogger));
+app.use(cors());
 app.use(express.json());
 
 // app.use((reg, res, next) => {
