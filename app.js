8eb3cab88f2ca7a433343bfd875f67f14d27a30a
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2023-05-05 22:32:59 +0300

    add readme file and eslint

--- app.js
+++ app.js
@@ -19,8 +19,4 @@ app.get("/", (reg, res) => {
   res.send("Hello Word");
 });
 
-// app.listen(3000, () => {
-//   console.log("example app listening on port 3000!");
-// });
-
 module.exports = app;
