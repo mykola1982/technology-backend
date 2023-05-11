ed1dd474c4b17214aaf14d1570e4e68273d01608
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2023-05-11 22:32:14 +0300

    created controller updateByIdProduct

--- controllers/products/updateByIdProduct.js
+++ controllers/products/updateByIdProduct.js
@@ -1,3 +1,21 @@
-const updateByIdProduct = async (req, res, next) => {};
+const { Product } = require("../../models");
+
+const { HttpError } = require("../../helpers");
+
+const updateByIdProduct = async (req, res, next) => {
+  const { id } = req.params;
+
+  const product = await Product.findByIdAndUpdate(id, req.body, { new: true });
+
+  if (!product) {
+    throw HttpError(404, `Product with id:${id} was not found `);
+  }
+
+  res.status(200).json({
+    status: "success",
+    code: 200,
+    data: product,
+  });
+};
 
 module.exports = updateByIdProduct;
