bad988e5562da10232318390360c43f23b56cc7c
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2023-05-18 13:45:57 +0300

    added  check for of a number in controller updateByIdProduct

--- controllers/products/updateByIdProduct.js
+++ controllers/products/updateByIdProduct.js
@@ -4,17 +4,26 @@ const { HttpError } = require("../../helpers");
 
 const updateByIdProduct = async (req, res, next) => {
   const { id } = req.params;
+  const { number } = req.body;
 
-  const product = await Product.findByIdAndUpdate(id, req.body, { new: true });
+  const product = await Product.findOne({ number });
 
-  if (!product) {
+  if (product && product._id.toString() !== id) {
+    throw HttpError(409, "Product with this number is already in the database");
+  }
+
+  const updatedProduct = await Product.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+
+  if (!updatedProduct) {
     throw HttpError(404, `Product with id:${id} was not found `);
   }
 
   res.status(200).json({
     status: "success",
     code: 200,
-    data: product,
+    data: updatedProduct,
   });
 };
 
