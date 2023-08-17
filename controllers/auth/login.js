0c813649deb872067c4a70070571511ebb31a1f3
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2023-08-17 10:17:30 +0300

    fix

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -21,7 +21,7 @@ const login = async (req, res, next) => {
   }
 
   const payload = { id: user._id };
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "30s" });
 
   await User.findByIdAndUpdate(user._id, { token });
 
